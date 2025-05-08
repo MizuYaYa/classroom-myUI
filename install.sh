46426922f2dabb99e42e00fb006a73b08892d604
MizuYaYa <106097784+MizuYaYa@users.noreply.github.com>
2025-05-08 13:30:23 +0900

    chore: Chrome Web Store以外からインストールする方法を提供する

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cd ~/Library/Application\ Support/Google/Chrome/External\ Extensions/
+
+echo -n "{\"external_update_url\": \"https://clients2.google.com/service/update2/crx\"}" > fldjpemfcmojflhfcgldcbmpeodifbcf.json
---
+++ uninstall.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cd ~/Library/Application\ Support/Google/Chrome/External\ Extensions/
+
+rm fldjpemfcmojflhfcgldcbmpeodifbcf.json
