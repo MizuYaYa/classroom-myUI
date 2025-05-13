0c7e2af25aa2b010ac29a076e1e801060e7fe5ab
MizuYaYa <106097784+MizuYaYa@users.noreply.github.com>
2025-05-13 16:52:15 +0900

    feat: 課題情報表示用の要素を作成し、スタイルを追加

---
+++ src/createElement.js
@@ -0,0 +1,48 @@
+// @ts-check
+"use strict";
+
+/**
+ * @description 課題の下部に表示する要素を作成する
+ * @param {Omit<import("./script.js").descriptionData, "title">} infoData
+ */
+export function createInfoBarElementInStreamTab(infoData) {
+  const infoBarElement = document.createElement("div");
+  infoBarElement.classList.add("info-bar");
+
+  if (infoData.serviceName) {
+    const serviceNameElement = document.createElement("img");
+    // @ts-ignore
+    serviceNameElement.src = chrome.runtime.getURL("img/manufacturing.svg");
+
+    const serviceNameTooltipContainerElement = document.createElement("div");
+    serviceNameTooltipContainerElement.classList.add("tooltip");
+
+    const serviceNameTooltipElement = document.createElement("span");
+    serviceNameTooltipElement.classList.add("tooltip-text");
+    serviceNameTooltipElement.innerText = `${infoData.serviceName} を使用して投稿`;
+
+    serviceNameTooltipContainerElement.appendChild(serviceNameTooltipElement);
+    serviceNameTooltipContainerElement.appendChild(serviceNameElement);
+    infoBarElement.appendChild(serviceNameTooltipContainerElement);
+  }
+
+  const authorPElement = document.createElement("p");
+  authorPElement.innerText = infoData.author;
+
+  infoBarElement.appendChild(authorPElement);
+
+  return infoBarElement;
+}
+
+/**
+ * @description 課題の情報スペースに表示する要素を作成する
+ * @param {Omit<import("./script.js").descriptionData, "title">} infoData
+ */
+export function createInfoElementInClassTab(infoData) {
+  const infoElement = document.createElement("div");
+  infoElement.classList.add("cx8Okb", "dDKhVc");
+
+  infoElement.innerText = `投稿者: ${infoData.author} ${infoData.serviceName ? `(${infoData.serviceName})` : ""}`;
+
+  return infoElement;
+}
--- src/script.js
+++ src/script.js
@@ -1,6 +1,8 @@
 //@ts-check
 "use strict";
 
+import { createInfoBarElementInStreamTab, createInfoElementInClassTab } from "./createElement.js";
+
 const link = document.createElement("link");
 link.rel = "stylesheet";
 // @ts-ignore
@@ -18,9 +20,49 @@ const textClassElements = document.getElementsByClassName("JvYRu");
 // 授業毎の課題リストの要素識別
 const taskListId = '[jscontroller="BRRzA"]';
 
+/**
+ * @typedef descriptionData
+ * @property {string} author 投稿者
+ * @property {string} [serviceName] 使用サービス名
+ * @property {string} title 課題タイトル
+ */
+
+/**
+ * @param {string} description
+ * @returns {descriptionData | null}
+ */
+function parseClassDescription(description) {
+  const まほうの正規表現 =
+    /^(?<author>.+) さんが(?: (?<serviceName>.+) を使用して)?新しい課題を投稿しました: (?<title>.+)$/;
+  const match = description.match(まほうの正規表現);
+
+  if (!match || !match.groups) return null;
+
+  return {
+    author: match.groups.author,
+    serviceName: match.groups.serviceName,
+    title: match.groups.title,
+  };
+}
+
 function addClassInStreamTab() {
   for (const element of parentElement) {
     element.classList.add("set-height");
+
+    // ここでelementの子要素descriptionを取得
+    /** @type {HTMLSpanElement | null} */
+    const description = element.querySelector("span.asQXV");
+    if (!description) throw new Error("課題説明文の要素が見つかりませんでした。");
+
+    // 取得したdescriptionをparseClassDescriptionに渡す
+    const parsedDescription = parseClassDescription(description.innerText);
+    if (!parsedDescription) throw new Error("課題説明文が正しく解析できませんでした。");
+
+    description.innerText = parsedDescription.title;
+
+    const infoBarElement = createInfoBarElementInStreamTab(parsedDescription);
+
+    element.parentElement?.appendChild(infoBarElement);
   }
 
   for (const element of textElements) {
@@ -31,7 +73,26 @@ function addClassInStreamTab() {
 
 function addClassInClassTab() {
   for (const element of ClassParentElement) {
+    if (element.classList.contains("set-height")) continue;
     element.classList.add("set-height");
+
+    // ここでelementの子要素descriptionを取得
+    /** @type {HTMLDivElement | null} */
+    const description = element.querySelector("div.JvYRu");
+    if (!description) throw new Error("課題説明文の要素が見つかりませんでした。");
+
+    const parsedDescription = parseClassDescription(description.innerText);
+    if (!parsedDescription) throw new Error("課題説明文が正しく解析できませんでした。");
+
+    description.innerText = parsedDescription.title;
+
+    const infoElement = createInfoElementInClassTab(parsedDescription);
+
+    const infoSpaceElement = element.parentElement?.querySelector(".RL2eP");
+    if (!infoSpaceElement) throw new Error("課題説明文の兄弟要素である情報要素が見つかりませんでした。");
+    infoSpaceElement.classList.add("flex-column");
+
+    infoSpaceElement.appendChild(infoElement);
   }
 
   for (const element of textClassElements) {
